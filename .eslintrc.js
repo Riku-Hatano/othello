d094a18693e6e207189a09e3c0c8291accf110f6
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2024-01-26 03:04:40 +0900

    fix: .eslintrの設定を変更しました

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
         }
     ],
     "parserOptions": {
-        "ecmaVersion": "latest",
+        "ecmaVersion": 2021,
         "sourceType": "module"
     },
     "plugins": [
